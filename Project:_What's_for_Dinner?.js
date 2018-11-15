fe74606d19534e1c01b3677d2181845b06ad81ee
TomNWilson <42974128+TomNWilson@users.noreply.github.com>
2018-11-15 11:07:45 -0500

    Create Project:_What's_for_Dinner?.js

---
+++ Project:_What's_for_Dinner?.js
@@ -0,0 +1,20 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+fill(255, 243, 163); //bun colour
+rect(85, 175, 235, 66); //bun shape
+arc(86, 208, 24, 65, 91, 270);
+arc(320, 208, 24, 66, -90, 91);
+
+fill(199, 32, 32); //hotdog colour
+rect(79, 189, 248, 37); //hotdog
+arc(81, 208, 39, 37, 91, 270);
+arc(325, 208, 39, 37, -90, 91);
+
+strokeWeight(5);
+stroke(255, 234, 0); //mustard colour
+line(79, 213, 328, 210); //mustard colour
+
+stroke(255, 0, 0); //ketchup colour
+line(79, 205, 328, 203); //ketchup
