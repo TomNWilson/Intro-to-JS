f253568f6279ffd18457dfd2f11d7d4e98e6ec1d
TomNWilson <42974128+TomNWilson@users.noreply.github.com>
2018-11-15 11:10:01 -0500

    Create Project:_Ad_Design.js

---
+++ Project:_Ad_Design.js
@@ -0,0 +1,23 @@
+//product name
+fill(144, 127, 156);
+textSize(35);
+text("Arm Appendages", 10, 30);
+
+//product info
+fill(135, 69, 179);
+textSize(15);
+text("Don't miss out on the chance to:", 10, 70);
+text("  - Bang the bungo", 10, 87);
+text("  - Wave to the consuming abyss", 10, 104);
+text("  - Pilfer from the pin men", 10, 121);
+
+//drawing of appendage
+strokeWeight(3);
+stroke(144, 127, 156);
+line(38, 201, 160, 305);
+line(160, 305, 250, 270);
+line(250, 270, 266, 248);
+line(250, 270, 282, 245);
+line(250, 270, 296, 253);
+line(250, 270, 293, 267);
+line(250, 270, 265, 283);
